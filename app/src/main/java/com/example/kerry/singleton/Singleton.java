01654770b2a0a05214dc3c38a6b2ab3be45b6c8a
Kerry Dong <kerry50407@gmail.com>
2016-05-28 20:23:54 +0800

    Singleton 2nd version

--- app/src/main/java/com/example/kerry/singleton/Singleton.java
+++ app/src/main/java/com/example/kerry/singleton/Singleton.java
@@ -4,13 +4,14 @@ package com.example.kerry.singleton;
  * Created by Kerry on 2016/5/27.
  */
 public class Singleton {
-    private static Singleton instance;
+    private static Singleton sInstance;
     private Singleton() {}
 
-    public static synchronized Singleton getInstance() {
-        if(instance == null) {
-            instance = new Singleton();
-        }
-        return instance;
+    public static Singleton getInstance() {
+        return SingletonHolder.sInstance;
+    }
+
+    private static class SingletonHolder {
+        private static final Singleton sInstance = new Singleton();
     }
 }
